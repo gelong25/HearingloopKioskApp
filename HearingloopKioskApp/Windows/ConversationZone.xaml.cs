0e539b797900aba34ad6a510e3fc1f43cc24ba3c
hayeon <johayeon788@gmail.com>
2024-06-20 15:12:10 +0900

    window 추가 및 ResourceDictionary를 활용한 스타일 재사용성 증가

---
+++ HearingloopKioskApp/Windows/ConversationZone.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+
+namespace HearingloopKioskApp.Windows
+{
+    public partial class ConversationZone : Window
+    {
+        private SpeechToText speechToText;
+
+        public ConversationZone()
+        {
+            InitializeComponent();
+            speechToText = new SpeechToText();
+            speechToText.textbox1 = TextBox1;
+            speechToText.textbox2 = TextBox2;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            speechToText.OnEnable();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            speechToText.OnDisable();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
